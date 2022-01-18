b915a03341e04d8e26e69f8177b91339ea57327a
AndrewSimons3 <simdrew07@gmail.com>
2022-01-18 08:08:21 -0600

    initialize game, makeMove and nextTurn function initialized

---
+++ main.js
@@ -0,0 +1,11 @@
+import Game from './Game.js';
+
+const game = new Game();
+
+console.log(game.board);
+console.log("My turn is", game.turn);
+game.nextTurn();
+console.log('My turn is', game.turn);
+game.makeMove(3);
+console.log(game.board);
+
