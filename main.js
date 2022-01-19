fc054f52c3a9cd520b46f36f9ab41df3b6de3336
AndrewSimons3 <simdrew07@gmail.com>
2022-01-19 06:38:34 -0600

    added gameview class and updateboard logic

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import Game from './Game.js';
+import GameView from './GameView.js'
 
 const game = new Game();
+const gameView = new GameView();
+gameView.updateBoard(game);
 
 console.log(game.board);
 console.log("My turn is", game.turn);
@@ -8,4 +11,8 @@ game.nextTurn();
 console.log('My turn is', game.turn);
 game.makeMove(3);
 console.log(game.board);
+gameView.updateBoard(game);
+game.nextTurn();
+game.makeMove(6);
+gameView.updateBoard(game);
 
