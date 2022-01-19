5a00d269c0b1400ea658c5eec33f4f35dca27515
AndrewSimons3 <simdrew07@gmail.com>
2022-01-19 08:04:12 -0600

    added color style for winning tiles

--- js/GameView.js
+++ js/GameView.js
@@ -5,15 +5,22 @@ export default class GameView {
 
 	updateBoard(game) {
 		this.updateTurn(game);
+		const winningCombination = game.findWinningCombinations();
 		console.log('this is a board within gameview');
 		console.log(game.board);
 		for (let i = 0; i < game.board.length; i++) {
 			const tile = document.querySelector(`.board-tile[data-index='${i}']`);
 			tile.textContent = game.board[i];
 
+			tile.classList.remove('tile-winner');
+
 			let tileType = game.board[i] == 'X' ? 'tile-x' : 'tile-o';
 
 			tile.innerHTML = `<span class="${tileType}">${game.board[i] ? game.board[i] : ''}</span>`
+
+			if (winningCombination && winningCombination.includes(i)) {
+				tile.classList.add('tile-winner');
+			}
 		}
 	}
 
